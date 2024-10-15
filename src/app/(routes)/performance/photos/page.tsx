978ad447ed16014d910303f090cd2914dcc206c3
page1597 <gimm08377@gmail.com>
2024-10-16 01:35:26 +0900

    Style: performance/photos 페이지 초기 레이아웃

--- src/app/(routes)/performance/photos/page.tsx
+++ src/app/(routes)/performance/photos/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import src from "../../../../../public/images/waterjet.jpg";
+import Image from "next/image";
+import styles from "./page.module.scss";
 
 export default function Photos() {
-  return <div>회사 시공 사진 목록 페이지</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles["image-container"]}>
+        <Image src={src} alt="alt" width={300} className={styles.image} />
+        <Image src={src} alt="alt" width={300} className={styles.image} />
+        <Image src={src} alt="alt" width={300} className={styles.image} />
+        <Image src={src} alt="alt" width={300} className={styles.image} />
+        <Image src={src} alt="alt" width={300} className={styles.image} />
+      </div>
+    </div>
+  );
 }
