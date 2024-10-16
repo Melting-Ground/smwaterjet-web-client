b91f1cb69c434ec9e6fe596342ed9d93ba189ab6
page1597 <gimm08377@gmail.com>
2024-10-16 23:42:38 +0900

    Style: performance/photos 사진 호버 시 애니메이션 추가

--- src/app/(routes)/performance/photos/page.tsx
+++ src/app/(routes)/performance/photos/page.tsx
@@ -1,17 +1,67 @@
+"use client";
 import React from "react";
-import src from "../../../../../public/images/waterjet.jpg";
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { motion } from "framer-motion";
+
+interface ImageItem {
+  id: number;
+  description?: string;
+  src: string;
+  alt?: string;
+}
 
 export default function Photos() {
+  const imageList: ImageItem[] = [
+    {
+      src: "/images/waterjet.jpg",
+      alt: "alt",
+      id: 1,
+      description: "gkgk",
+    },
+    {
+      src: "/images/waterjet.jpg",
+      alt: "alt",
+      id: 1,
+      description: "gkgk",
+    },
+    {
+      src: "/images/waterjet.jpg",
+      alt: "alt",
+      id: 1,
+      description: "gkgk",
+    },
+    {
+      src: "/images/waterjet.jpg",
+      alt: "alt",
+      id: 1,
+      description: "gkgk",
+    },
+    {
+      src: "/images/waterjet.jpg",
+      alt: "alt",
+      id: 1,
+      description: "gkgk",
+    },
+  ];
+
   return (
     <div className={styles.container}>
-      <div className={styles["image-container"]}>
-        <Image src={src} alt="alt" width={400} className={styles.image} />
-        <Image src={src} alt="alt" width={400} className={styles.image} />
-        <Image src={src} alt="alt" width={400} className={styles.image} />
-        <Image src={src} alt="alt" width={400} className={styles.image} />
-        <Image src={src} alt="alt" width={400} className={styles.image} />
+      <div className={styles.gallery}>
+        {imageList.map((image) => (
+          // link 등
+          <div key={image.id} className={styles["img-container"]}>
+            <motion.div whileHover={{ scale: 1.08 }}>
+              <Image
+                src={image.src}
+                alt={image.alt || ""}
+                width={400}
+                height={300}
+                className={styles.image}
+              />
+            </motion.div>
+          </div>
+        ))}
       </div>
     </div>
   );
