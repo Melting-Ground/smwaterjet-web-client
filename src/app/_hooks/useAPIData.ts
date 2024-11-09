0ab3d87e6bf5807986bd01ef76cc40d38c718253
page1597 <gimm08377@gmail.com>
2024-11-09 17:05:29 +0900

    Feat: useAPIData 수정
    - 게시판 리스트 조회 시 페이지 추가 및 문의사항 비밀번호 전달

--- src/app/_hooks/useAPIData.ts
+++ src/app/_hooks/useAPIData.ts
@@ -3,29 +3,40 @@ import axiosInstance from "../_config/axiosInstance";
 import { getAuthHeaders } from "../_utils/getToken";
 import { APIConfig } from "../_config/apiConfig";
 
-export const useAPIData = <T>(apiConfig: APIConfig<T>) => {
+export const useAPIData = <T>(apiConfig: APIConfig<T>, page?: number) => {
   const [dataList, setDataList] = useState<T[]>([]);
   const [dataDetail, setDataDetail] = useState<T | undefined>(undefined);
 
-  const fetchDataList = async () => {
+  const fetchDataList = async (page: number) => {
+    console.log(apiConfig.url);
     try {
-      const response = await axiosInstance.get(apiConfig.url);
+      const response = await axiosInstance.get(`${apiConfig.url}?page=${page}`);
       setDataList(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error("fetchDataList 에러", error);
     }
   };
 
   useEffect(() => {
-    fetchDataList();
-  }, []);
+    fetchDataList(page ?? 1);
+  }, [page]);
 
-  const fetchData = async (id: string) => {
+  const fetchData = async (id: string, password?: string) => {
+    // password O: 문의사항
+    // password X: 공지사항
+    // console.log(password);
+    // const password = "1234";
     try {
-      const response = await axiosInstance.get(`${apiConfig.url}/${id}`);
+      const response = await axiosInstance({
+        method: password ? "post" : "get",
+        url: `${apiConfig.url}/${id}`,
+        data: password ? { password } : undefined,
+      });
+      console.log(response);
       setDataDetail(response.data);
     } catch (error) {
-      console.error("fetData 에러, error");
+      console.error(`fetData 에러, ${error}`);
     }
   };
 
