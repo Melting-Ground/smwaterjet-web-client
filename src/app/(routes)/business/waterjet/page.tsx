ebc500768a223eeedf65fb23a278ab3c6695c2ec
page1597 <gimm08377@gmail.com>
2024-11-26 22:01:41 +0900

    Style: business/waterjet 스타일링 수정중

--- src/app/(routes)/business/waterjet/page.tsx
+++ src/app/(routes)/business/waterjet/page.tsx
@@ -5,11 +5,12 @@ import Image from "next/image";
 import backgroundImage from "../../../../../public/images/background-image.jpg";
 
 export default function Waterjet() {
+  // TODO: 시멘틱 태그 수정하기
   return (
     <div className={styles.container}>
       <section id="definition">
+        <h2 className={styles["sr-only"]}>워터젯(WaterJet) 공법</h2>
         <div className={styles["dark-section"]}>
-          <h3 className={styles["sr-only"]}>워터젯(WaterJet) 공법</h3>
           <p className={styles.label}>
             <em>워터젯(WaterJet) 공법</em>이란 물을 초고압, 초고속 상태의
             제트루로 만들어
@@ -83,84 +84,49 @@ export default function Waterjet() {
         <div className={styles["card-background"]}>
           <ol>
             <li>
-              <span className={styles.number}>
-                0<em>1</em>
-              </span>
+              <span className={styles.number}>1</span>
               <p>
                 분진과 진동이 없어 시끄럽거나 까다로운 작업 조건에 적합합니다.
               </p>
             </li>
             <li>
-              <span className={styles.number}>
-                0<em>2</em>
-              </span>
+              <span className={styles.number}>2</span>
               <p>
-                충격과 진동이 없어 인근 구조물에
-                <br />
-                균열 등의 영향을 주지 않습니다.
+                충격과 진동이 없어 인근 구조물에 균열 등의 영향을 주지 않습니다.
               </p>
             </li>
             <li>
-              <span className={styles.number}>
-                0<em>3</em>
-              </span>
+              <span className={styles.number}>3</span>
               <p>
-                철근의 손상 없이 콘크리트의 부식된 부위만을 선별하여 제거할 수
-                있습니다.
+                철근의 손상 없이 콘크리트 부식 부위만을 선별하여 제거합니다.
               </p>
             </li>
             <li>
-              <span className={styles.number}>
-                0<em>4</em>
-              </span>
-              <p>
-                재래식 공법에 비해 시공 속도가
-                <br />
-                매우 빠릅니다.
-              </p>
+              <span className={styles.number}>4</span>
+              <p>재래식 공법에 비해 시공 속도가 매우 빠릅니다.</p>
             </li>
             <li>
-              <span className={styles.number}>
-                0<em>5</em>
-              </span>
+              <span className={styles.number}>5</span>
               <p>
-                압의의 방향으로 절단이 가능하며,
-                <br />
-                수중에서도 절단이 가능합니다.
+                압의의 방향으로 절단이 가능하며, 수중에서도 절단이 가능합니다.
               </p>
             </li>
             {/* 오타 확인 */}
             <li>
-              <span className={styles.number}>
-                0<em>6</em>
-              </span>
+              <span className={styles.number}>6</span>
               <p>
-                표면을 요철 방식으로 제거하므로
-                <br />
-                보강 콘크리트의 부착력이 증대됩니다.
+                요철 방식으로 표면을 제거해 보강 콘크리트의 부착력이 증대됩니다.
               </p>
             </li>
             <li>
-              <span className={styles.number}>
-                0<em>7</em>
-              </span>
+              <span className={styles.number}>7</span>
               <p>
-                노즐을 소형화, 경량화, 소비력화함으로써
-                <br />
-                로보트화가 용이합니다.
+                노즐을 소형화, 경량화, 소비력화함으로써 로보트화가 용이합니다.
               </p>
             </li>
             <li>
-              <span className={styles.number}>
-                0<em>8</em>
-              </span>
-              <p>
-                파쇄되지 않는 면에 대한 충격이 없어
-                <br />
-                구조물의 성능을 그대로 유지하면서
-                <br />
-                파쇄할 수 있습니다.
-              </p>
+              <span className={styles.number}>8</span>
+              <p>파쇄되지 않는 면의 구조물을 유지하며 파쇄할 수 있습니다.</p>
             </li>
           </ol>
         </div>
