b6258e3e0b9bdf5ea1fa110d7dad9de642502e45
page1597 <gimm08377@gmail.com>
2024-11-26 21:57:36 +0900

    Feat: 경로 수정
    - /business/cutting, /business/chipping 제거 및 /buisiness/fields 추가

--- src/app/(routes)/business/chipping/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Chipping() {
-  return <div>Chipping</div>;
-}
--- src/app/(routes)/business/cutting/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Cutting() {
-  return <div>Cutting</div>;
-}
