ccddb946bf52eb052d93d0395822476358188d33
page1597 <gimm08377@gmail.com>
2024-11-27 15:44:34 +0900

    Style: 스타일 수정 (business/waterjet 워터젯 공법 특징)

--- src/app/(routes)/business/waterjet/page.tsx
+++ src/app/(routes)/business/waterjet/page.tsx
@@ -46,20 +46,34 @@ export default function Waterjet() {
                 보수 공법 중 재료에 대한 부분은 상당한 발전과 개발이
                 이루어졌지만,
                 <br />
-                가장 중요한 불량한 부분을 제거하는 방법은 여전히
-                <br /> 기능공의 판단이나 능력에 의존하는 낙후된 방식만이
-                사용되고 있습니다.
+                가장 중요한 불량한 부분을 제거하는 방법은 여전히 기능공의
+                판단이나 능력에
+                <br /> 의존하는 낙후된 방식만이 사용되고 있습니다.
               </p>
               <br />
               <p>
                 현재 우리나라를 비롯한 선진국에서는 콘크리트 취약부를 제거하는
                 방법으로
                 <br />
-                워터젯 공법만을 사용하도록 규정하고 있습니다.
+                <em>워터젯 공법</em>만을 사용하도록 규정하고 있습니다.
                 <br />
                 이는 시공 중 환경적인 요소와 시공 후 제품의 품질이 뛰어난 것으로
                 <br />
                 여러 시험을 통해 입증되었기 때문입니다.
+              </p>
+              <br />
+              <p>
+                <em>워터젯 공법</em>은{" "}
+                <strong>철근을 전혀 손상시키지 않고 해체</strong>하므로,
+                <br />
+                표면 위에 보강 공사를 시행할 때 보강 콘크리트의 부착력을
+                증대시킬 수 있습니다.
+              </p>
+              <p>
+                따라서 <strong>도로 보수 공사</strong>나{" "}
+                <strong>교량의 상판 및 교량 하부 보수 공사</strong> 등에서
+                구조물을 해체하지 않고도 보강이 필요한 열화된 콘크리트 부분만을
+                제거하는 데 매우 탁월합니다.
               </p>
             </div>
             <Image
@@ -74,13 +88,13 @@ export default function Waterjet() {
       <section className={styles.feature} id="feature">
         <div className={styles["feature-title-container"]}>
           <h3 className={styles["sub-title"]}>워터젯 공법의 특징</h3>
-          <p className={styles.content}>
+          {/* <p className={styles.content}>
             일정한 압력의 물을 각도있게 분사하여 재료의 공극을 파고들어 취약부를
             제거하며
             <br />
             작업이 완료 후에 잔존하는 구조물은 일정한 강도 이상을 반드시 지니게
             됩니다.
-          </p>
+          </p> */}
         </div>
         <div className={styles["card-background"]}>
           <ol>
