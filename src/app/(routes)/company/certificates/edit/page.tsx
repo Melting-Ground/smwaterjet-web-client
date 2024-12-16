ed2ed65a5d4424ffd0841679811618b229fb3797
page1597 <gimm08377@gmail.com>
2024-12-17 01:02:02 +0900

    Style: certificate 작성 페이지 스타일링

--- src/app/(routes)/company/certificates/edit/page.tsx
+++ src/app/(routes)/company/certificates/edit/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useState } from "react";
-import styles from "./page.module.scss";
+// import styles from "./page.module.scss";
+import styles from "@/_layout/support/edit/layout.module.scss";
+
 import Input from "@/_components/Input/Input";
 import Button from "@/_components/Button/Button";
 import useFormData from "@/_hooks/useFormData";
 import { API_URLS } from "@/_config/apiConfig";
+import useBoardAction from "@/_hooks/useBoardAction";
 
 export default function Edit() {
   const CERTIFICATE_API = API_URLS.certificates;
@@ -19,6 +22,7 @@ export default function Edit() {
     typeof API_URLS.certificates.method.get,
     typeof API_URLS.certificates.method.post
   >(API_URLS.certificates, certificate, setCertificate);
+  const { goToListPage } = useBoardAction("company", "certificates");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -30,6 +34,7 @@ export default function Edit() {
 
     try {
       await uploadForm(e);
+      goToListPage();
     } catch (error) {
       console.error("에러", error);
     }
@@ -50,15 +55,25 @@ export default function Edit() {
 
   return (
     <div className={styles.container}>
-      <button onClick={() => console.log(certificate)}>확인</button>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <label htmlFor="file">파일 선택</label>
-        <input type="file" name="files" id="file" onChange={handleFileChange} />
+        <Input
+          type="file"
+          name="files"
+          id="file"
+          onChange={handleFileChange}
+          className={styles["file-input"]}
+        />
         <label htmlFor="file">제목</label>
-        <Input name="title" id="title" onChange={handleTitleChange} />
-        <Button type="submit" color="primary">
-          업로드
-        </Button>
+        <Input name="title" id="title" onChange={handleTitleChange} fullWidth />
+        <div className={styles["button-container"]}>
+          <Button type="button" color="primary-border" onClick={goToListPage}>
+            취소
+          </Button>
+          <Button type="submit" color="primary">
+            작성 완료
+          </Button>
+        </div>
       </form>
     </div>
   );
